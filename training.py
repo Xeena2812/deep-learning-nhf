319329dc174e4d2541f6bfce3ab6a1205c06721e
bence0012 <bence7123@gmail.com>
2022-12-08 11:24:35 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,12 +1,11 @@
-import glob
-import imageio
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import PIL
+
 from tensorflow.keras import Sequential
 from tensorflow.keras import layers
-import time
+
 import tensorflow as tf
 import keras
 from keras.models import Model
